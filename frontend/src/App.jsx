3e9c9c285dfedf3e9ccf780e4e931eb12d0d4234
Nguyen Chi Loc <chiloc07102002@gmail.com>
2026-09-25 21:21:05 +0700

    feat: add login page and navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
+
 import HomePage from './pages/HomePage'
+import Login from './pages/Login'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+
       <ToastContainer
         position="top-right"
         autoClose={2800}

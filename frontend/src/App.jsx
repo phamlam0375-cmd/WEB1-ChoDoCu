f084b712664dab64806c4c93a29bc1277b024467
Dangfind <vuhoanginhdang@gmail.com>
2026-09-25 13:51:30 +0700

    demo-v1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <main id="center">
-      <span className="counter">Website Chợ Đồ Cũ</span>
-      <h1>Demo CI/CD thành công</h1>
-      <p>Giao diện này được kiểm tra bởi CI và triển khai tự động bằng Docker.</p>
-      <code>feature branch → master → CI → CD → Docker</code>
-    </main>
+    <h1>
+      HELOOOOOOOOOOOOOOOOOOOO
+    </h1>
   )
 }
 
